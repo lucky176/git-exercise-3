add76325459b90191ab0f6692d78f966a57c2351
urverma152@gmail.com <urverma152@gmail.com>
2021-06-10 15:26:05 +0530

    Third commit

---
+++ Data/coreDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using core_mvc_git_exercise.model;
+
+namespace core_mvc_git_exercise.Data
+{
+    public class coreDbContext : DbContext
+    {
+        public coreDbContext (DbContextOptions<coreDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<core_mvc_git_exercise.model.Player> Player { get; set; }
+    }
+}
