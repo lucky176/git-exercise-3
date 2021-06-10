99ad17319b0be54baba6beed806f5b261a8904a4
urverma152@gmail.com <urverma152@gmail.com>
2021-06-10 15:06:14 +0530

    Second commit

---
+++ model/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace core_mvc_git_exercise.model
+{
+    [Table("Player")]
+    public class Player
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Team { get; set; }
+
+    }
+}
